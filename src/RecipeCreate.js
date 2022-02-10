233342f4f88b7ef50042b90acacdbef347587ea3
Blkthor23 <JayDaKing23@gmail.com>
2022-02-10 10:16:41 -0600

    fixed issue with create, added additional functionality

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,40 +1,94 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-function RecipeCreate({createRecipe}) {
-  const [form, setForm] = useState({})
-  const handleChange = ({target}) => {
-    setForm({
-      ...form,
-      [target.name]:target.value,
-    })
-  }
+function RecipeCreate({ createRecipe }) {
+  const namesRef = useRef();
+  const cusineRef = useRef();
+  const urlRef = useRef();
+  const ingredientRef = useRef();
+  const prepRef = useRef();
+  const [form, setForm] = useState({});
+
+  const handleChange = ({ target }) => {
+    const item = target.value.trim();
+    if (item.length > 0) {
+      setForm({
+        ...form,
+        [target.name]: item,
+      });
+    }
+  };
   const initRecipe = {
     name: "",
     cuisine: "",
     photo: "",
     ingredients: "",
     preparation: "",
-  }
-  
+  };
+
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const item = event.target.value
-    if(item.length > 0 && item.trim()) {
-    createRecipe(form)
-    setForm({...initRecipe})
-    }
-  }
-  
+    event.preventDefault();
+    createRecipe(form);
+    setForm({ ...initRecipe });
+    namesRef.current.value = null;
+    cusineRef.current.value = null;
+    urlRef.current.value = null;
+    ingredientRef.current.value = null;
+    prepRef.current.value = null;
+  };
+
   return (
     <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
-            <td><input type="text" name="name" placeholder="Name" required={true} onChange={handleChange}></input ></td>
-            <td><input type="text" name="cuisine" placeholder="cuisine" onChange={handleChange} required={true}></input></td>
-            <td><input type="url" name="photo" placeholder="URL" onChange={handleChange}></input></td>
-            <td><textarea name="ingredients" placeholder="Ingredient" onChange={handleChange} required={true}></textarea></td>
-            <td><textarea name="preparation" placeholder="Preparation" onChange={handleChange} required={true}></textarea></td>
+            <td>
+              <input
+                ref={namesRef}
+                type="text"
+                name="name"
+                placeholder="Name"
+                required={true}
+                onChange={handleChange}
+              ></input>
+            </td>
+            <td>
+              <input
+                ref={cusineRef}
+                type="text"
+                name="cuisine"
+                placeholder="cuisine"
+                onChange={handleChange}
+                required={true}
+              ></input>
+            </td>
+            <td>
+              <input
+                ref={urlRef}
+                type="url"
+                name="photo"
+                placeholder="URL"
+                onChange={handleChange}
+                required={true}
+              ></input>
+            </td>
+            <td>
+              <textarea
+                ref={ingredientRef}
+                name="ingredients"
+                placeholder="Ingredient"
+                onChange={handleChange}
+                required={true}
+              ></textarea>
+            </td>
+            <td>
+              <textarea
+                ref={prepRef}
+                name="preparation"
+                placeholder="Preparation"
+                onChange={handleChange}
+                required={true}
+              ></textarea>
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
